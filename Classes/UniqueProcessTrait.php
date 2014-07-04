edfc3f1e842b8b7dee08b5ff02d86123bfa622a1
Борис Зискин <bmziskin@gmail.com>
2014-07-04 21:19:58 +0400

    trait для проверки уникальности процесса

---
+++ Classes/UniqueProcessTrait.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace zis\DaemonBundle\Classes;
+
+/**
+ * Trait UniqueProcessTrait
+ */
+trait UniqueProcessTrait
+{
+    protected $pidFile;
+
+    protected function setPidFile($file)
+    {
+        $this->pidFile = $file;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDaemonActive()
+    {
+        if (!$this->pidFile) {
+            throw new \Exception('Set pid file location');
+        }
+        if (is_file($this->pidFile)) {
+            $pid = file_get_contents($this->pidFile);
+            //проверяем на наличие процесса
+            if (posix_kill($pid, 0)) {
+                //демон уже запущен
+                return true;
+            } else {
+                //pid-файл есть, но процесса нет
+                if (!unlink($this->pidFile)) {
+                    //не могу уничтожить pid-файл. ошибка
+                    exit(-1);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     *
+     */
+    public function putPidFile()
+    {
+        if (!$this->pidFile) {
+            throw new \Exception('Set pid file location');
+        }
+        file_put_contents($this->pidFile, getmypid());
+    }
+
+
+
+} 
